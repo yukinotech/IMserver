c215cf50ede5b98e7d10e60cd47ac149c340f716
yukinotech <you@example.com>
2020-03-30 16:00:30 +0800

    finish login register

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const session = require("express-session")
+const MongoStore = require('connect-mongo')(session)
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+let app = express()
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(session({
+  secret: 'this is string key', // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
+  name: 'session_id',
+  /*保存在本地cookie的一个名字 默认connect.sid  可以不设置*/
+  resave: false,
+  /*强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false。*/
+  saveUninitialized: true, //强制将未初始化的 session 存储。  默认值是true  建议设置成true
+  cookie: {
+    maxAge: 1000 * 60 * 30 /*过期时间*/
+  },
+  /*secure https这样的情况才可以访问cookie*/
+
+  //设置过期时间比如是30分钟，只要游览页面，30分钟没有操作的话在过期
+
+  rolling: true, //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+  store: new MongoStore({
+    url:'mongodb://localhost:27017',
+    dbName: 'IMdb',
+    collection:'sessions'
+  })
+}))
+
+client.connect((err)=>{
+  app.post("/login",async(req,res)=>{
+    // req.session.userInfo='张三'
+    console.log(req.sessionID)
+    console.log(req.body)
+    console.log(req.session.username)
+    if(req.body && req.body.username && req.body.password){
+        let username = req.body.username
+        let password = req.body.password
+        let uPattern = /^[a-zA-Z0-9_-]{4,16}$/
+        if(uPattern.test(username) && uPattern.test(password)){
+            let db = client.db('IMdb');
+            let collection = db.collection('users');
+            let findOneResult = await collection.findOne({username,password})
+            // console.log('findOneResult:',findOneResult)
+            if(findOneResult===null){
+              console.log('username or password wrong')
+              res.json({ status: 201 })
+            } else{
+              console.log('user login success')
+              req.session.username=username
+              res.json({ status: 200 })
+            }
+        } else{
+          console.log('202 error')
+          res.json({ status: 202 })
+        }
+    } else{
+      console.log('202 error')
+      res.json({ status: 202 })
+    }
+  });
+  
+  app.post("/register",async (req,res)=>{
+    // console.log(req)
+    // console.log(req.cookies)
+    console.log(req.sessionID)
+    console.log(req.body)
+    console.log(req.session.username)
+    if(req.body){
+      if(req.body.username && req.body.password){
+        let username = req.body.username
+        let password = req.body.password
+        let uPattern = /^[a-zA-Z0-9_-]{4,16}$/
+        if(uPattern.test(username) && uPattern.test(password)){
+            let db = client.db('IMdb');
+            let collection = db.collection('users');
+            let findOneResult = await collection.findOne({username})
+            // console.log('findOneResult:',findOneResult)
+            if(findOneResult===null){
+              let insertOneResult = await collection.insertOne({username,password})
+              // console.log('insertOneResult:',insertOneResult)
+              console.log('user register success ')
+              res.json({ status: 200 })
+            } else{
+              console.log('user already in ')
+              res.json({ status: 201 })
+            }
+        } else{
+          res.json({ status: 202 })
+        }
+      } else{
+        res.json({ status: 202 })
+      }
+    } else{
+      res.json({ status: 202 })
+    }
+    console.log(req.session)
+    console.log(req.sessionID)
+    
+    // res.send('登录成功')
+
+  });
+  
+  
+  app.listen(3000,()=>{
+    console.log('server run on port 3000')
+  })
+})
