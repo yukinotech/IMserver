7eddd762b6fcd7b7d72fff3d43ffeb41089b967e
yukinotech <you@example.com>
2020-04-10 20:20:56 +0800

    add mongo config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = function(argv){
   if(argv[2]==='-pro'){
     return {
-      mongoUrl:'mongodb://admin:zyf456@0.0.0.0:27017'
+      mongoUrl:'mongodb://admin:zyf456@localhost:27017'
     }
   } else{
     return {
