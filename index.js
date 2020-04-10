29e01e532e054a8b125befa5c66339c78c764c2d
yukinotech <you@example.com>
2020-04-10 20:00:46 +0800

    add mongo config

--- index.js
+++ index.js
@@ -31,49 +31,41 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
+let sessionMiddleware = session({
+  secret: 'this is string key', // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
+  name: 'session_id',
+  /*保存在本地cookie的一个名字 默认connect.sid  可以不设置*/
+  resave: false,
+  /*强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false。*/
+  saveUninitialized: true, //强制将未初始化的 session 存储。  默认值是true  建议设置成true
+  cookie: {
+    maxAge: 1000 * 60 * 30 /*过期时间*/
+  },
+  /*secure https这样的情况才可以访问cookie*/
+
+  //设置过期时间比如是30分钟，只要游览页面，30分钟没有操作的话在过期
+
+  rolling: true, //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+  store: new MongoStore({
+    url:url,
+    dbName: 'IMdb',
+    collection:'sessions'
+  })
+})
+
+app.use(sessionMiddleware)
+
+io.use(function(socket, next) {
+  // console.log('每次都跑？')
+  sessionMiddleware(socket.request, socket.request.res, next);
+})
+
 //双向hash表，存socket-io-id和username
 let hashName ={}
 
 client.connect((err)=>{
   
-  let db = client.db('IMdb')
-  db.createCollection('users')
-  db.createCollection('sessions')
-  db.createCollection('messages')
 
-  let sessionMiddleware = session({
-    secret: 'this is string key', // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
-    name: 'session_id',
-    /*保存在本地cookie的一个名字 默认connect.sid  可以不设置*/
-    resave: false,
-    /*强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false。*/
-    saveUninitialized: true, //强制将未初始化的 session 存储。  默认值是true  建议设置成true
-    cookie: {
-      maxAge: 1000 * 60 * 30 /*过期时间*/
-    },
-    /*secure https这样的情况才可以访问cookie*/
-  
-    //设置过期时间比如是30分钟，只要游览页面，30分钟没有操作的话在过期
-  
-    rolling: true, //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
-    store: new MongoStore({
-      url:url,
-      dbName: 'IMdb',
-      collection:'sessions'
-    })
-  })
-  
-  app.use(sessionMiddleware)
-  // app.use((res,req,next)=>{
-  //   console.log('express每次都')
-  //   next()
-  // })
-  io.use(function(socket, next) {
-    // console.log('每次都跑？')
-    sessionMiddleware(socket.request, socket.request.res, next);
-  })
-  
-  
   app.post("/login",loginHandler(client));
   
   app.post("/register",registerHandler(client));
