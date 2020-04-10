84603d128cdddc2a9788de5429414b8b092f60d9
yukinotech <you@example.com>
2020-04-10 20:29:35 +0800

    add mongo config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 module.exports = function(argv){
   if(argv[2]==='-pro'){
     return {
-      mongoUrl:'mongodb://admin:zyf456@localhost:27017'
+      // mongoUrl:'mongodb://admin:zyf456@localhost:27017'
+      mongoUrl:'mongodb://localhost:27017'
     }
   } else{
     return {
